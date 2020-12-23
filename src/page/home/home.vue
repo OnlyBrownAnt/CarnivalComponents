c7438b47e663922600937c43243933322396f5fc
zhangyao2 <zhangyao2@sunline.cn>
2020-12-23 10:04:32 +0800

    增加page文件夹存放作为页面的组件，新建home组件

---
+++ src/page/home/home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <span>home</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+  props: {
+  },
+  created () {
+  },
+  computed: {
+  },
+  methods: {
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,24 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
-
+import home from '../page/home/home.vue';
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/', // 默认页面重定向到主页
-      redirect: '/HelloWorld' // 重定向
+      redirect: '/home' // 重定向
     },
     {
       path: '/HelloWorld',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: home
     }
   ]
 });
