c34a21b864aa13d72f4bce8606d2cf29fb7d61a8
zhangyao2 <zhangyao2@sunline.cn>
2020-12-23 09:34:32 +0800

    修改ESLint的规则: 关闭驼峰校验规则(因为项目中使用的后端接口具有下划线)

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,11 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 是否强制结尾添加分号，0 => 可有可无， 1 => 没有则警告， 2 => 必须有
+    'semi': ['error', 'always'],
+    // 关闭camelcase驼峰校验规则
+    'camelcase': 'off',
+    '@typescript-eslint/camelcase': 0
   }
 }
